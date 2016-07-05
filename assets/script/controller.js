a647148a009b9f329df258a6d91f0662aaa641ae
Aaron <newcomer.aaron@icloud.com>
2016-07-05 16:36:48 -0400

    added styles

--- assets/script/controller.js
+++ assets/script/controller.js
@@ -2,14 +2,14 @@ wittyApp.controller('gameController', function($scope, $http){
 forward = 0;
 side = 0
 var board = [
- ['a sign reading "Make your move"','green grass','ogre camp','temple of angles','sad code student','igloo','Gnome tinkerer','a coupoun for 1 free potion'],
- ['a mysterious bottle','day old soup','a nice but probably evil looking witch','gingerbread house','a magical sword','large rocks','river','The Shire'],
- ['Westeros','DRAGON!','A Dire Wolf','Jon Snow','Vampire','Wildling','Merlin','dirty boots and the ashes of their owner'],
- ['A sign saying LEAVE','The Ghost of a former lover','A bow and Single Arrow','a small keg of ale','Axes','Lumber','2 gold teeth','30 non-gold teeth'],
- ['A shadowy figure','a deck of cards','An empty bottle of wine','a large field','A rusted bucket','A wild horse','a small Kitty',' a small puppy'],
- ['An apple tree','20 feet of rope','small pouch of coins','a fire','a freshly killed Cornish Game hen','a unreadable tome','a slightly more readable tome','a picture book'],
+ ['a sign reading "Make your move"',' some green grass','an ogre camp','a temple of angles','a sad coding student','igloo','a gnome tinkerer','a coupoun for 1 free potion'],
+ ['a mysterious bottle','day old soup','a nice, but probs evil, looking witch','a gingerbread house','a magical sword','some large rocks','a river','The Shire'],
+ ['Westeros','A DRAGON!','a dire wolf','Jon Snow','a vampire','a wildling','Merlin','dirty boots and the ashes of their owner'],
+ ['a sign saying "LEAVE"','the ghost of a former lover','a bow and a single arrow','a small keg of ale','axes','lumber','2 gold teeth','30 non-gold teeth'],
+ ['a shadowy figure','a deck of cards','an empty bottle of wine','a large field','a rusted bucket','a wild horse','a small Kitty',' a small puppy'],
+ ['an apple tree','20 feet of rope','a small pouch of coins','a fire','a freshly killed Cornish Game hen','a unreadable tome','a slightly more readable tome','a picture book'],
  ['a tattered wall of glyphs','a glowing orb','a shield','a drawing of a future you','a bag of "Lord Sanders spices"','14 pebbles in a cirle','a map of an island with a rather large X','Nun-Chucks'],
- ['a box of gloves','a flase sense of hope','stone tablet with 10...rules?','a lute','a sundial','large eggs in a nest','Nessie','a pond'] ];
+ ['a box of gloves','a false sense of hope','stone tablet with 10...rules?','a lute','a sundial','large eggs in a nest','Nessie','a pond'] ];
 $scope.board = board[forward][side];
 $scope.rollFunc= function(){
   $scope.diceMoveForward= Math.floor((Math.random() * 3));
@@ -20,11 +20,13 @@ $scope.rollFunc= function(){
   if (forward + $scope.diceMoveForward <= 7 &&  side + $scope.diceMoveSide <= 7){
       forward += $scope.diceMoveForward;
       side += $scope.diceMoveSide;
+      $scope.cantMove = "";
   }
   else if (forward + $scope.diceMoveForward > 7 || side + $scope.diceMoveSide > 7){
     forward = forward -  $scope.diceMoveForward;
     side = side - $scope.diceMoveSide;
-    console.log("back")
+    console.log("back");
+    $scope.cantMove = "";
   }
 
 };
